6e87d1262ed84c825d2f2f7362ee0dacc37090b6
Błażej Łach <blazej.lach@circlekeurope.com>
2019-07-16 15:38:53 +0200

    Robbot pattern initial sample setup
    Add sample for robot pattern in android espresso tests.

---
+++ robbot_pattern/app/src/main/java/pl/bl/robotpatterntests/ui/MainActivity.kt
@@ -0,0 +1,13 @@
+package pl.bl.robotpatterntests.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import pl.bl.robotpatterntests.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
